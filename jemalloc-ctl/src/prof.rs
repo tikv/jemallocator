47a5d9a461a56d2f88647b143a2482d9f9a1e68d
Yuhao Su <yuhaosu@outlook.com>
2023-07-19 20:06:59 +0800

    add dump and prefix
    Signed-off-by: Yuhao Su <yuhaosu@outlook.com>

---
+++ jemalloc-ctl/src/prof.rs
@@ -0,0 +1,47 @@
+//! `jemalloc`'s profiling utils.
+
+option! {
+    dump[ str: b"prof.dump\0", str: 2 ] => &'static str |
+    ops: w |
+    docs:
+    /// Dump a memory profile to the specified file, or if NULL is specified, 
+    /// to a file according to the pattern <prefix>.<pid>.<seq>.m<mseq>.heap, 
+    /// where <prefix> is controlled by the opt.prof_prefix and prof.prefix options.
+    ///
+    /// # Examples
+    ///
+    /// ```
+    /// # #[global_allocator]
+    /// # static ALLOC: tikv_jemallocator::Jemalloc = tikv_jemallocator::Jemalloc;
+    /// #
+    /// # fn main() {
+    /// use tikv_jemalloc_ctl::prof;
+    /// let dump = prof::dump::mib().unwrap();
+    /// prof.write("prof.heap").unwrap());
+    /// # }
+    /// ```
+    mib_docs: /// See [`dump`].
+}
+
+option! {
+    prefix[ str: b"prof.prefix\0", str: 2 ] => &'static str |
+    ops: w |
+    docs:
+    /// Set the filename prefix for profile dumps. See opt.prof_prefix for the default setting. 
+    /// 
+    /// This can be useful to differentiate profile dumps such as from forked processes.
+    ///
+    /// # Examples
+    ///
+    /// ```
+    /// # #[global_allocator]
+    /// # static ALLOC: tikv_jemallocator::Jemalloc = tikv_jemallocator::Jemalloc;
+    /// #
+    /// # fn main() {
+    /// use tikv_jemalloc_ctl::prof;
+    /// let prefix = prof::prefix::mib().unwrap();
+    /// prefix.write("my_prefix").unwrap());
+    /// # }
+    /// ```
+    mib_docs: /// See [`prefix`].
+}
