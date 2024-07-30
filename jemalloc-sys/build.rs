8766f1167657bb3ea4183b9b3083158c2f036e52
roblabla <unfiltered@roblab.la>
2024-07-30 11:04:12 +0200

    Add support for win7 targets
    Signed-off-by: roblabla <unfiltered@roblab.la>

--- jemalloc-sys/build.rs
+++ jemalloc-sys/build.rs
@@ -401,7 +401,9 @@ fn gnu_target(target: &str) -> String {
         //
         // See https://github.com/jemalloc/jemalloc/blob/5.3.0/configure.ac#L750
         "i686-pc-windows-msvc" => "i686-pc-mingw32".to_string(),
+        "i686-win7-windows-msvc" => "i686-pc-mingw32".to_string(),
         "x86_64-pc-windows-msvc" => "x86_64-pc-mingw32".to_string(),
+        "x86_64-win7-windows-msvc" => "x86_64-pc-mingw32".to_string(),
         "i686-pc-windows-gnu" => "i686-w64-mingw32".to_string(),
         "x86_64-pc-windows-gnu" => "x86_64-w64-mingw32".to_string(),
         "armv7-linux-androideabi" => "arm-linux-androideabi".to_string(),
