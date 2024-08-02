5efb1303e4f5ce9870536134859f19fca29d0881
roblabla <unfiltered@roblab.la>
2024-08-02 15:57:06 +0200

    Add windows targets to NO_UNPREFIXED_MALLOC_TARGET
    It turns out, jemalloc doesn't support having unprefixed malloc on these
    targets. When --with-jemalloc-prefix is not specified, jemalloc will
    implicitly add a `je_` prefix regardless.
    
    Signed-off-by: roblabla <unfiltered@roblab.la>

--- jemalloc-sys/src/env.rs
+++ jemalloc-sys/src/env.rs
@@ -18,7 +18,12 @@ pub static NO_BG_THREAD_TARGETS: &[&str] = &["musl"];
 // “it was found that the `realpath` function in libc would allocate with libc malloc
 //  (not jemalloc malloc), and then the standard library would free with jemalloc free,
 //  causing a segfault.”
+//
 // https://github.com/rust-lang/rust/commit/e3b414d8612314e74e2b0ebde1ed5c6997d28e8d
 // https://github.com/rust-lang/rust/commit/9f3de647326fbe50e0e283b9018ab7c41abccde3
 // https://github.com/rust-lang/rust/commit/ed015456a114ae907a36af80c06f81ea93182a24
-pub static NO_UNPREFIXED_MALLOC_TARGETS: &[&str] = &["android", "dragonfly", "darwin"];
+//
+//  Furthermore, macos (using macho abi) and windows (using pecoff abi) don't
+//  support unprefixed malloc at all:
+//  https://github.com/jemalloc/jemalloc/blob/8dc97b11089be6d58a52009ea3da610bf90331d3/configure.ac#L1109
+pub static NO_UNPREFIXED_MALLOC_TARGETS: &[&str] = &["android", "dragonfly", "darwin", "windows"];
