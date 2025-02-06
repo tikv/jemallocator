fa4486d23f7402f3999b8c857333c60090001314
Konrad <vesphy@gmail.com>
2025-02-06 04:26:54 +0100

    don't pass CFLAGS and fix _FORTIFY_SOURCE errors (#116)
    Signed-off-by: Konrad Stepniak <me@th7nder.net>

--- jemalloc-sys/build.rs
+++ jemalloc-sys/build.rs
@@ -145,17 +145,9 @@ fn main() {
         println!("cargo:rustc-link-lib={}={}", kind, &stem[3..]);
         return;
     }
-    // Disable -Wextra warnings - jemalloc doesn't compile free of warnings with
-    // it enabled: https://github.com/jemalloc/jemalloc/issues/1196
-    let compiler = cc::Build::new().extra_warnings(false).get_compiler();
-    let cflags = compiler
-        .args()
-        .iter()
-        .map(|s| s.to_str().unwrap())
-        .collect::<Vec<_>>()
-        .join(" ");
+
+    let compiler = cc::Build::new().get_compiler();
     info!("CC={:?}", compiler.path());
-    info!("CFLAGS={:?}", cflags);
 
     assert!(out_dir.exists(), "OUT_DIR does not exist");
     let jemalloc_repo_dir = PathBuf::from("jemalloc");
@@ -190,9 +182,6 @@ fn main() {
     )
     .current_dir(&build_dir)
     .env("CC", compiler.path())
-    .env("CFLAGS", cflags.clone())
-    .env("LDFLAGS", cflags.clone())
-    .env("CPPFLAGS", cflags)
     .arg(format!("--with-version={je_version}"))
     .arg("--disable-cxx")
     .arg("--enable-doc=no")
