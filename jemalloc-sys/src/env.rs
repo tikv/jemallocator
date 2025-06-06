9bc0fd939c0387593bec6a65af0a703571c83a45
Mads Marquart <mads@marquart.dk>
2025-06-06 14:22:36 +0200

    Make unprefixed consistently override the system allocator
    Signed-off-by: Mads Marquart <mads@marquart.dk>

--- jemalloc-sys/src/env.rs
+++ jemalloc-sys/src/env.rs
@@ -21,4 +21,4 @@ pub static NO_BG_THREAD_TARGETS: &[&str] = &["musl"];
 // https://github.com/rust-lang/rust/commit/e3b414d8612314e74e2b0ebde1ed5c6997d28e8d
 // https://github.com/rust-lang/rust/commit/9f3de647326fbe50e0e283b9018ab7c41abccde3
 // https://github.com/rust-lang/rust/commit/ed015456a114ae907a36af80c06f81ea93182a24
-pub static NO_UNPREFIXED_MALLOC_TARGETS: &[&str] = &["android", "dragonfly", "darwin"];
+pub static NO_UNPREFIXED_MALLOC_TARGETS: &[&str] = &["android", "dragonfly", "apple"];
