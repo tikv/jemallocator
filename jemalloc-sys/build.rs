4a0b928b72411d5c2703ae35e5d11eb089264b29
hl <hl@nmcsoft.com>
2026-07-23 09:02:09 +0800

    fix Windows MSVC jemalloc target triple

--- jemalloc-sys/build.rs
+++ jemalloc-sys/build.rs
@@ -448,8 +448,8 @@ fn execute(cmd: &mut Command, on_fail: impl FnOnce()) {
 
 fn gnu_target(target: &str) -> String {
     match target {
-        "i686-pc-windows-msvc" => "i686-pc-win32".to_string(),
-        "x86_64-pc-windows-msvc" => "x86_64-pc-win32".to_string(),
+        "i686-pc-windows-msvc" => "i686-pc-mingw32".to_string(),
+        "x86_64-pc-windows-msvc" => "x86_64-pc-mingw32".to_string(),
         "i686-pc-windows-gnu" | "i686-pc-windows-gnullvm" => "i686-w64-mingw32".to_string(),
         "x86_64-pc-windows-gnu" | "x86_64-pc-windows-gnullvm" => "x86_64-w64-mingw32".to_string(),
         "aarch64-pc-windows-gnullvm" => "aarch64-w64-mingw32".to_string(),
